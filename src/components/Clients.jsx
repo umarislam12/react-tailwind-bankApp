d7ef6db0a26bb5330e25e45bd6cf6215443f8157
umarislam12 <f14b150@ibitpu.edu.pk>
2023-06-21 20:11:43 +0500

    leaving footer for next publish

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -8,7 +8,7 @@ const Clients = () =>
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {clients.map((client)=>(
           <div className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`} key={client.id}>
-              <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain' />
+              <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain hover:brightness-200' />
           </div>
         ))}
       </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import styles from '../style'
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from '../constants'
 
-const Footer = () => {
-  return (
+const Footer = () =>  (
     <div>Footer</div>
   )
-}
+
 
 export default Footer
